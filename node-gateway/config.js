b0209e98d396738c208a1a271393976530e63a1d
vlad000011 <105325266+vlad000011@users.noreply.github.com>
2025-12-12 07:46:37 +0300

    Update config.js

--- node-gateway/config.js
+++ node-gateway/config.js
@@ -1,7 +1,8 @@
+
 module.exports = {
-  SOAP_WSDL: process.env.SOAP_WSDL || 'http://localhost:8080/php-legacy/wsdl/library.wsdl',
-  SOAP_ENDPOINT: process.env.SOAP_ENDPOINT || 'http://localhost:8080/php-legacy/soap-server.php',
-  REPORT_RAW_URL: process.env.REPORT_RAW_URL || 'http://localhost:8080/php-legacy/report.php?raw=1',
+  SOAP_WSDL: 'http://localhost/library/backend-php-legacy/wsdl/library.wsdl',
+  SOAP_ENDPOINT: 'http://localhost/library/backend-php-legacy/soap-server.php',
+  REPORT_RAW_URL: 'http://localhost/library/backend-php-legacy/report.php?raw=1',
   PORT: process.env.PORT || 3000,
-  LOWDB_FILE: process.env.LOWDB_FILE || __dirname + '/lowdb.json'
-}
+  LOWDB_FILE: __dirname + '/lowdb.json'
+};
